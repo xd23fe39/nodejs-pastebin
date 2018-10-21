7493e8341ea5000db5b9ecdbe404d11aec4a89f8
xd23fe39 <xd23fe39@yahoo.de>
2018-10-21 10:13:57 +0200

    Restful SAmple Server Code aktualisiert

--- restfull-sample/src/rest-server.js
+++ restfull-sample/src/rest-server.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 
 var express = require('express');
+var os = require('os');
 var app = express();
 var pug = require('pug');
 var fs = require("fs");
+const tutorial = "http://www.tutorialspoint.com/nodejs/";
+
 
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
+      // console.log( data );
+      res.end( data );
    });
 })
 
 app.get('/', function (req, res) {
   fs.readFile( __dirname + "/" + "info.html", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
+    res.end( data );
   });
 })
 
@@ -41,10 +43,12 @@ app.get('/hello', function (req, res) {
 
 var server = app.listen(8081, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = os.hostname();
+  var port = server.address().port;
 
-  console.log("\n  Data Directory: " + __dirname + "\n\n");
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("\nRESTfull Sample Application on " + os.platform() + "/" + os.arch());
+  console.log("See also: " + tutorial + "\n");
+  console.log("\n  Document Root: " + __dirname + "\n\n");
+  console.log("Server listening at http://%s:%s\n\n", host, port);
 
 })
